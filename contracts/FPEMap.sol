11b0a5d89c78fcea6b1160d427eb4dc8ed06cbd1
0xEstarriol <Estarriol.Vetch@protonmail.com>
2022-05-13 11:16:24 -0400

    use 3 round instaead

--- contracts/FPEMap.sol
+++ contracts/FPEMap.sol
@@ -10,7 +10,7 @@ library FPEMap {
     string constant DOMAIN_ERROR_MSG = "The FPE domain should be within the domain of the Fiestel network (domain <= 2 ** size)";
     string constant INPUT_OUTSIDE_DOMAIN_ERROR_MSG = "input is not within the domain";
 
-    uint256 constant DEFAULT_ROUND = 4;
+    uint256 constant DEFAULT_ROUND = 3;
 
     function fpeMappingFeistel(uint256 input, uint256 key, uint256 round, uint256 size, uint256 domain) internal pure returns (uint256 output) {
         require(input < domain, INPUT_OUTSIDE_DOMAIN_ERROR_MSG);
